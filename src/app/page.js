6d07fdec141f349723fb77b652106c1881a20a5d
Jeruzael <dumale.jeruzael.bscs2019@gmail.com>
2024-01-20 22:55:10 +0800

    Deployment

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,7 @@ const Show = (b) => {
   }
 }
 
+
 const hi = "Hi! I\'m"
 const intro = "I\’m a frontend developer from the Philippines. I design and build web applications based on the highest standards Implementing new technologies that help your product stand out to competitors."
 
